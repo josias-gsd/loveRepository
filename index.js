bd4fd9782c1e9f7260526b3fe2dbff40258137f8
Josias Gentil <josiasgentil7@gmail.com>
2025-11-03 23:18:00 +0000

    outro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import RouterNota from "./Routers/router.js";
+
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", RouterNota);
+
+app.listen(9002, () => console.log("Servidor Voando 🚀"));
