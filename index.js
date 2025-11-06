dcd940d3b762cf79178dfd0574ac37842cf495be
Josias Gentil <josiasgentil7@gmail.com>
2025-11-06 07:49:26 +0000

    actualizado love-11

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://josiasaby.netlify.app"); // ou "http://localhost:5173"
+  res.header("Access-Control-Allow-Origin", "*"); // ou "http://localhost:5173"
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
