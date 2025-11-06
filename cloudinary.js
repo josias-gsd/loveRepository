a8cc395fb842f0e9975f9c0f9922baacb51f751d
Josias Gentil <150050395+josias-gsd@users.noreply.github.com>
2025-11-06 21:45:49 +0000

    backend love-14

--- cloudinary.js
+++ cloudinary.js
@@ -2,8 +2,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name:process.env.CLOUD_NAME,
-  api_key: process.env.api_key,
-  api_secret: process.env.api_secret,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
