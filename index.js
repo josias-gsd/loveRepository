2db2b09b49ce0d25bdf6f99fb191d9da88011a2c
Josias Gentil <josiasgentil7@gmail.com>
2025-11-06 07:55:03 +0000

    actualizado love-12

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ import RouterNota from "./Routers/router.js";
 
 const app = express();
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // ou "http://localhost:5173"
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+app.use(
+  cors({
+    origin: "https://josiasaby.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+app.use(express.json());
 
-  next();
-});
 app.use(express.json());
 
 app.use("/", RouterNota);
